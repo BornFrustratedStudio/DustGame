56973682a89d6611c67b37a0bfc483885e04c6de
giosaffi <saffirio.giorgio@gmail.com>
2018-12-16 12:20:16 +0100

    - Added Recipe

--- Assets/Project/ScriptableObjects/Recipe.cs
+++ Assets/Project/ScriptableObjects/Recipe.cs
@@ -1,9 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-public class Recipe : MonoBehaviour
+
+[CreateAssetMenu()]
+public class Recipe : ScriptableObject
 {
+    [SerializeField]
+    private string      m_name;
+    [SerializeField]
+    private Rarity      m_rarity;
+    [SerializeField]
+    private float       m_dropRate;
+    [SerializeField]
+    private List<Item>  m_recipeItems;
 
+    public string       Name        { get { return m_name;      } }
+    public Rarity       Rarity      { get { return m_rarity;    } }
+    public float        DropRate    { get { return m_dropRate;  } }
+    public List<Item>   RecipeItems
+    {
+        get 
+        {
+            if(m_recipeItems == null)
+                m_recipeItems = new List<Item>();
 
-
-
+            return m_recipeItems;
+        }
+    }
 }
